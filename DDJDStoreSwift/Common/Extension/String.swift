9c68ec0303d62032cc2f92c83c522512c5aea301
hao peng <haopeng@haodemacbook-air.local>
2018-04-28 16:48:08 +0800

    //提交

---
+++ DDJDStoreSwift/Common/Extension/String.swift
@@ -0,0 +1,22 @@
+//
+//  String.swift
+//  DDJDStoreSwift
+//
+//  Created by hao peng on 2018/4/27.
+//  Copyright © 2018年 zldd. All rights reserved.
+//
+
+import Foundation
+extension String {
+    ///截取字符串 重第几位开始
+    public func substring(from index: Int) -> String {
+        if self.count > index {
+            let startIndex = self.index(self.startIndex, offsetBy: index)
+            let subString = self[startIndex..<self.endIndex]
+
+            return String(subString)
+        } else {
+            return self
+        }
+    }
+}
